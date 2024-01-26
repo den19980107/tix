695e4da75b996b93bb156ea20cee6de3b39bc0ed
paul.tseng <paul.tseng@advantech.com.tw>
2024-01-26 17:33:23 +0800

    add rwd

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -6,7 +6,7 @@ export default function BookingPage() {
     <Alert>
       <RocketIcon className="h-4 w-4" />
       <AlertTitle>選擇想預定的票種</AlertTitle>
-      <AlertDescription>
+      <AlertDescription className="w-full">
         你可以透過選單選擇不同票種
       </AlertDescription>
     </Alert>
