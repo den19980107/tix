ec4ca399ba271529bb41a94b5e21bea271f6136b
paul.tseng <paul.tseng@advantech.com.tw>
2024-01-26 14:41:40 +0800

    update ui

--- app/components/thsrc-ticket-list.tsx
+++ app/components/thsrc-ticket-list.tsx
@@ -1,11 +1,31 @@
-import prisma from "@/lib/prisma"
+'use client'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
+import { ThsrcTicket } from "@/types/thsrc-ticket"
 import ThsrcCard from "../booking/components/thsrc-card"
 
-export default async function ThsrcTicketList() {
-  const orders = await prisma.order.findMany()
+type ThsrcTicketListProps = {
+  orders: ThsrcTicket[]
+}
+export default function ThsrcTicketList({ orders }: ThsrcTicketListProps) {
   return (
-    <div className="flex flex-col">
-      {orders.map(order => <ThsrcCard ticket={order}></ThsrcCard>)}
-    </div>
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>高鐵訂單 ({orders.length})</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Carousel className="m-10">
+          <CarouselContent>
+            {orders.map(order => (
+              <CarouselItem className="md:basis-1/3 lg:basis-1/3">
+                <ThsrcCard ticket={order}></ThsrcCard>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </CardContent>
+    </Card>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import prisma from "@/lib/prisma"
 import ThsrcTicketList from "./components/thsrc-ticket-list"
 import PageHeader from "./page-header"
 
-export default function IndexPage() {
+export default async function IndexPage() {
+  const orders = await prisma.order.findMany()
   return (
     <div className="hidden space-y-6 p-10 pb-16 md:block">
       <PageHeader title="首頁" subTitle="一個訂票服務" ></PageHeader>
-      <ThsrcTicketList></ThsrcTicketList>
+      <ThsrcTicketList orders={orders}></ThsrcTicketList>
     </div>
   )
 }
