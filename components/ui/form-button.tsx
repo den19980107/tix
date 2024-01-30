9bb4d411356f4cc3f994ee2d9b3040f425716290
paul.tseng <paul.tseng@advantech.com.tw>
2024-01-30 17:03:11 +0800

    fix ref not assign

--- components/ui/form-button.tsx
+++ components/ui/form-button.tsx
@@ -5,10 +5,10 @@ import { useFormStatus } from 'react-dom'
 import { Button, ButtonProps } from './button'
 
 
-export const FormButton = React.forwardRef<HTMLButtonElement, ButtonProps>(({ children, ...props }) => {
+export const FormButton = React.forwardRef<HTMLButtonElement, ButtonProps>(({ children, ...props }, ref) => {
   const { pending } = useFormStatus()
   return (
-    <Button type="submit" disabled={pending} {...props}>
+    <Button type="submit" disabled={pending} ref={ref} {...props}>
       {pending ? <div className="animate-spin"><LoaderIcon /></div> : children}
     </Button>
   )
