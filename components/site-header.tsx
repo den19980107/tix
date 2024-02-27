1230a3dd6eaa544f2069f772c3279411e5c9bc84
paul.tseng <paul.tseng@advantech.com.tw>
2024-02-27 08:50:59 +0800

    fix: add avatar

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,8 +4,9 @@ import { siteConfig } from "@/config/site"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 import { signOut, useSession } from "next-auth/react"
-import { LogOutIcon, UserIcon } from "lucide-react"
+import { LogOutIcon } from "lucide-react"
 import { Button } from "./ui/button"
+import { Avatar, AvatarFallback } from "./ui/avatar"
 
 export function SiteHeader() {
   const { data: session } = useSession()
@@ -16,10 +17,11 @@ export function SiteHeader() {
         <MainNav items={siteConfig.mainNav} isLogin={!!session} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            {session &&
+            {session && session.user && session.user.username && session.user.username.length > 0 &&
               <>
-                <UserIcon></UserIcon>
-                <span>{session?.user?.username}</span>
+                <Avatar>
+                  <AvatarFallback>{session.user?.username[0].toUpperCase()}</AvatarFallback>
+                </Avatar>
                 <Button variant="ghost" onClick={() => signOut()}>
                   <LogOutIcon></LogOutIcon>
                 </Button>
