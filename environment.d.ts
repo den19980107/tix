7be69e2a7fdcff79f8fb15169c2bebe39548c350
paul.tseng <paul.tseng@advantech.com.tw>
2024-03-29 14:46:13 +0800

    feat: add env type and add a deploy script for building docker image and push to docker hub

---
+++ environment.d.ts
@@ -0,0 +1,10 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_URL: string;
+    NEXTAUTH_SECRET: string;
+    NEXTAUTH_URL: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    TIX_WORKER_URL: string;
+  }
+}
