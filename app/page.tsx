cd2acab50aaf079acb3215118efcdf01e0f671a0
paul.tseng <paul.tseng@advantech.com.tw>
2024-04-02 21:07:26 +0800

    test: auto rebuild

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function IndexPage() {
 
   return (
     <div className="space-y-6 py-10 md:block md:p-10">
-      <PageHeader title="首頁" subTitle="一個訂票服務" ></PageHeader>
+      <PageHeader title="首頁 123" subTitle="一個訂票服務" ></PageHeader>
       {
         orders.length === 0
           ?
