7c5e3d0ec4b2d09c254335c49b852b56bb0f07c2
paul.tseng <paul.tseng@advantech.com.tw>
2024-04-11 10:06:20 +0800

    test

--- middleware.ts
+++ middleware.ts
@@ -10,5 +10,5 @@ export default withAuth({
 })
 
 export const config = {
-  matcher: ["/", "/booking/:path*", "/setting/:path*", "/order/:path*"]
+  matcher: ["/:path*", "/booking/:path*", "/setting/:path*", "/order/:path*"]
 }
